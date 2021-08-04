c13e129e93479e2ccf930094f444e7ae2d9e1a4a
vincenteof <vincenteof@qq.com>
2021-08-04 15:08:54 +0800

    feat: more list

--- ninetynine/working_with_lst.ml
+++ ninetynine/working_with_lst.ml
@@ -1,30 +1,30 @@
-(* Write a function last : 'a list -> 'a option that returns the last element of a list. *)
+(* 1. Write a function last : 'a list -> 'a option that returns the last element of a list. *)
 let rec last = function
   | [] -> None
   | h :: [] -> Some h
   | _ :: t -> last t
 
 
-(* Find the last but one (last and penultimate) elements of a list. *)
+(* 2. Find the last but one (last and penultimate) elements of a list. *)
 let rec last_two = function
   | [] | [_] -> None
   | [x; y] -> Some (x, y)
   | _ :: t -> last_two t
 
 
-(* Find the K'th element of a list. *)
+(* 3. Find the K'th element of a list. *)
 let rec at k = function
   | [] -> None
   | h :: t -> if k = 1 then Some h else at (k - 1) t
 
 
-(* Find the number of elements of a list *)
+(* 4. Find the number of elements of a list. *)
 let rec length = function
   | [] -> 0
   | _ :: t -> length t + 1
 
 
-(* Reverse a list *)
+(* 5. Reverse a list. *)
 (* 
   simple recursive implementation requires `append` operation,
   so we need change the calculation direction.  
@@ -34,3 +34,25 @@ let rev lst =
     | [] -> acc 
     | h :: t -> inner (h :: acc) t 
   in  inner [] lst
+
+
+(* 6. Find out whether a list is a palindrome. *)
+(*
+  `=` stands for structural equality while `==` stands for physical equality.
+*)
+let is_palindrome lst = lst = rev lst
+
+
+(* 7. Flatten a nested list structure. *)
+type 'a node =
+  | One of 'a 
+  | Many of 'a node list
+
+let rec flatten = function
+  | One x -> [x]
+  | Many [] -> []
+  | Many ((One x) :: t) -> x :: (flatten (Many t))
+  | Many ((Many l) :: t) -> (flatten (Many l)) @ (flatten (Many t))
+
+(* 8. Eliminate consecutive duplicates of list elements. *)
+
